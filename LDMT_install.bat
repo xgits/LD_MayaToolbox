147afeda1a18f71243a213d46482598310e8b0ca
unknown <xgits@outlook.com>
2018-05-05 17:20:06 +0800

    fixed?

--- LDMT_install.bat
+++ LDMT_install.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
- 
-echo d| xcopy /y /c ".\*" "C:\Users\%username%\Documents\maya\scripts\LD_MayaToolbox" /s 
-echo f| xcopy /y /c "C:\Users\%username%\Documents\maya\scripts\userSetup.py" "C:\Users\%username%\Documents\maya\scripts\userSetup_bak.py" /s 
-echo f| xcopy /y /c ".\userSetup.py" "C:\Users\%username%\Documents\maya\scripts\userSetup.py" /s 
+
+echo d| xcopy /y /c ".\*" "%userprofile%\Documents\maya\scripts\LD_MayaToolbox" /s 
+echo f| xcopy /y /c "%userprofile%\Documents\maya\scripts\userSetup.py" "%userprofile%\Documents\maya\scripts\userSetup_bak.py" /s 
+echo f| xcopy /y /c ".\userSetup.py" "%userprofile%\Documents\maya\scripts\userSetup.py" /s 
 pause
 exit
